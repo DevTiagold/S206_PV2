b0bd146800b3933cae15d47f17a582601cb329da
DevTiagold <tiagoaugustocostacarvalho@gmail.com>
2025-06-18 12:27:04 -0300

    Adicionando o report mochawesome --> cypress/reports/mochawesome.html

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
